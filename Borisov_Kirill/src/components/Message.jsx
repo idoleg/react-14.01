f5a27d162d7048fc366272b4054177afc5d94e21
Kirill Borisov <kirill-borisov@yandex.ru>
2020-01-24 19:53:49 +0300

    Borisov Kirill Lesson 2 Patch 1
    Borisov Kirill Lesson 2 Patch 1

--- Borisov_Kirill/src/components/Message.jsx
+++ Borisov_Kirill/src/components/Message.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 
 export default class Message extends React.Component {
-
-  state = {
-      name: this.props.name,
-      content: this.props.content
-  }
-  
-  render() {
-    return (
-      <div><strong>{this.state.name}:</strong> {this.state.content}</div>
-    );
-  }
+    render() {
+      return <div><strong>{this.props.name}:</strong> {this.props.content}</div>
+    }
 }
--- Borisov_Kirill/src/lesson2.jsx
+++ Borisov_Kirill/src/lesson2.jsx
@@ -2,15 +2,82 @@ import ReactDOM from "react-dom";
 import React from "react";
 import MessageList from "./components/MessageList";
 
-const messages = [
-  { name: "Фёдор Михайлович", content: "Вацап май френд?" },
-  { name: "Арсений", content: "Да помаленьку, всё нормально. Энд ю?" },
-  { name: "Фёдор Михайлович", content: "Вот решил изучить РеактJS" },
-  { name: "Арсений", content: "Чудить изволите, Фёдор Михайлович?" },
-  { name: "Фёдор Михайлович", content: "Изволю, Сеня, юр гад дэм райт" }
-];
+export default class App extends React.Component {
+  state = {
+    messages: [
+      { name: "Фёдор Михайлович", content: "Вацап май френд?" },
+      { name: "Арсений", content: "Да помаленьку, всё нормально. Энд ю?" },
+      { name: "Фёдор Михайлович", content: "Вот решил изучить РеактJS" },
+      { name: "Арсений", content: "Чудить изволите, Фёдор Михайлович?" },
+      { name: "Фёдор Михайлович", content: "Изволю, Сеня, юр гад дэм райт" }
+    ]
+  };
 
-ReactDOM.render(
-  <MessageList messages={messages} />,
-  document.getElementById("root")
-);
+  nextHumanIndex = -1;
+  nextHumanName = ["Владелец мыши", "Карус Бром", "Карл Фридрих Иероним"];
+  nextHumanContent = [
+    "Я нажал кнопку",
+    "О, шикарный суп наварили",
+    "Я знаю, вы благородный человек и в душе тоже против Англии."
+  ];
+  robotName = "Робот";
+
+  handleClick = () => {
+    this.nextHumanIndex =
+      this.nextHumanIndex >= this.nextHumanName.length - 1
+        ? (this.nextHumanIndex = 0)
+        : this.nextHumanIndex + 1;
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        {
+          name: this.nextHumanName[this.nextHumanIndex],
+          content: this.nextHumanContent[this.nextHumanIndex]
+        }
+      ]
+    });
+  };
+
+  componentDidUpdate = () => {
+    const lastMessage = this.state.messages[this.state.messages.length - 1];
+    if (lastMessage.name !== this.robotName) {
+      let robotAnswer = "";
+      switch (lastMessage.name) {
+        case "Владелец мыши":
+          robotAnswer = "Молодец, " + lastMessage.name + ". Так держать!";
+          break;
+        case "Карус Бром":
+          robotAnswer = "О, ВЕЛИКИЙ суп наварили";
+          break;
+        case "Карл Фридрих Иероним":
+          robotAnswer =
+            "Да, в душе против. Да, она мне не нравится. Но я сижу и помалкиваю!";
+          break;
+        default:
+          robotAnswer = "WTF is going on?";
+      }
+
+      robotAnswer = "Ответ для " + lastMessage.name + " - " + robotAnswer;
+
+      setTimeout(() => {
+        this.setState({
+          messages: [
+            ...this.state.messages,
+            { name: this.robotName, content: robotAnswer }
+          ]
+        });
+      }, 1000);
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>Отправить сообщение</button>
+        <MessageList messages={this.state.messages} />
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
